3cd3ef36d32cc60fae8d45aaa0e1e73cf269d1ab
Janis Balcers <frikoo@inbox.lv>
2026-04-22 23:40:30 +0300

    Pielikta muzika un klikski

---
+++ Assets/Scripts/SFXScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class SFXScript : MonoBehaviour
+{
+    public AudioSource sFXSource;
+    public AudioClip[] audioClip;
+
+    public void PlaySFX(int ix)
+    {
+        if (sFXSource.isPlaying)
+            sFXSource.Stop();
+
+        sFXSource.PlayOneShot(audioClip[ix]);
+    }
+}
